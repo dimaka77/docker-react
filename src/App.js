0e6bc40dd0fa5de3a5bb48f388debc421f88304f
DR <dimaka77@gmail.com>
2019-09-14 22:19:15 -0700

    Modified title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>What's up Yanusik?</p>
+        <p>Welcome to Dmitriy Romanenko test page.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
